0fb40b459b49af7ecd09557e1ec60e8e6198cc40
up866467 <44296360+up866467@users.noreply.github.com>
2020-04-02 03:00:40 +0100

    Matching game redesign, Signup has 2 inputs S/T

--- Games/Memory/memory.js
+++ Games/Memory/memory.js
@@ -52,7 +52,7 @@ function resetBoard() {
 
 (function shuffle() {
   cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
+    let randomPos = Math.floor(Math.random() * 16);
     card.style.order = randomPos;
   });
 })();
