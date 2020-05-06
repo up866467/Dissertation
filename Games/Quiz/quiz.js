4997fab66615268ca126e7d671c07358f3263b9f
up866467 <44296360+up866467@users.noreply.github.com>
2020-05-06 03:31:03 +0100

    updates

--- Games/Quiz/quiz.js
+++ Games/Quiz/quiz.js
@@ -1,154 +1,39 @@
-(function () {
-  // Functions
-  function buildQuiz(){
-    // variable to store the HTML output
-    const output = [];
+function check(){
 
-    // for each question...
-    myQuestions.forEach(
-      (currentQuestion, questionNumber) => {
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var correct = 0;
 
-        // variable to store the list of possible answers
-        const answers = [];
 
-        // and for each available answer...
-        for(letter in currentQuestion.answers){
+	if (question1 == "Yes") {
+		correct++;
+}
+	if (question2 == "Yes") {
+		correct++;
+}	
+	if (question3 == "Always") {
+		correct++;
+	}
 
-          // ...add an HTML radio button
-          answers.push(
-            `<label>
-              <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-            </label>`
-          );
-        }
+	var messages = ["Great job!", "That's just okay", "You really need to do better"];
+	var score;
 
-        // add this question and its answers to the output
-        output.push(
-          `<div class="slide">
-            <div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join("")} </div>
-          </div>`
-        );
-      }
-    );
+	if (correct == 0) {
+		score = 2;
+	}
 
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join('');
-  }
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
 
-  function showResults(){
+	if (correct == 3) {
+		score = 0;
+	}
 
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll('.answers');
+	document.getElementById("after_submit").style.visibility = "visible";
 
-    // keep track of user's answers
-    let numCorrect = 0;
-
-    // for each question...
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
-
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-      // if answer is correct
-      if(userAnswer === currentQuestion.correctAnswer){
-        // add to the number of correct answers
-        numCorrect++;
-
-        // color the answers green
-        answerContainers[questionNumber].style.color = 'lightgreen';
-      }
-      // if answer is wrong or blank
-      else{
-        // color the answers red
-        answerContainers[questionNumber].style.color = 'red';
-      }
-    });
-
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  }
-
-  function showSlide(n) {
-    slides[currentSlide].classList.remove('active-slide');
-    slides[n].classList.add('active-slide');
-    currentSlide = n;
-    if(currentSlide === 0){
-      previousButton.style.display = 'none';
-    }
-    else{
-      previousButton.style.display = 'inline-block';
-    }
-    if(currentSlide === slides.length-1){
-      nextButton.style.display = 'none';
-      submitButton.style.display = 'inline-block';
-    }
-    else{
-      nextButton.style.display = 'inline-block';
-      submitButton.style.display = 'none';
-    }
-  }
-
-  function showNextSlide() {
-    showSlide(currentSlide + 1);
-  }
-
-  function showPreviousSlide() {
-    showSlide(currentSlide - 1);
-  }
-
-  // Variables
-  const quizContainer = document.getElementById('quiz');
-  const resultsContainer = document.getElementById('results');
-  const submitButton = document.getElementById('submit');
-  const myQuestions = [
-    {
-        question: "Is this an example of a question?",
-        answers: 
-        a: "No",
-        b: "Maybe",
-        c: "Yes"
-      },
-      correctAnswer: "c"
-    },
-    {
-        question: "Is this an example of a question?",
-        answers: {
-        a: "No",
-        b: "Maybe",
-        c: "Yes"
-      },
-      correctAnswer: "c"
-    },
-    {
-        question: "Is this an example of a question?",
-        answers: {
-        a: "No",
-        b: "Maybe",
-        c: "Yes"
-      },
-      correctAnswer: "c"
-    }
-  ];
-
-  // Kick things off
-  buildQuiz();
-
-  // Pagination
-  const previousButton = document.getElementById("previous");
-  const nextButton = document.getElementById("next");
-  const slides = document.querySelectorAll(".slide");
-  let currentSlide = 0;
-
-  // Show the first slide
-  showSlide(currentSlide);
-
-  // Event listeners
-  submitButton.addEventListener('click', showResults);
-  previousButton.addEventListener("click", showPreviousSlide);
-  nextButton.addEventListener("click", showNextSlide);
-})();
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	}
+	
